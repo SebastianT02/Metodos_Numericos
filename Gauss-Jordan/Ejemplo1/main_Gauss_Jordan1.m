6f9356ba5e10886183073aef12ca8a1b7c06f2a1
Alejandro Romero <91906114+Alejandrop2906@users.noreply.github.com>
2021-10-28 11:45:09 -0500

    Update main_Gauss_Jordan1.m

--- Gauss-Jordan/Ejemplo1/main_Gauss_Jordan1.m
+++ Gauss-Jordan/Ejemplo1/main_Gauss_Jordan1.m
@@ -1,16 +1,16 @@
 % Autores: Alejandro Romero, Pablo Delgado, Sebastian Tacuri.
 %
-% Fecha: 2021/10/29
-% Descripcion: Se trata de una serie de algoritmos del álgebra lineal 
+% Fecha: 2021/10/28
+% Descripcion: Se trata de una serie de algoritmos del Ã¡lgebra lineal 
 %para determinar los resultados de un sistema de ecuaciones lineales.
 %
 % Problema 1:
 %   
-%Para la creación de un cubo de leds de 8x8x8 se adquirió un total de 780 unidades entre diodos 
-%leds, resistencias y cables de conexión, gastando un total de $249. El precio de un diodo les es 
-%de es de 0.50 centavos, el de una resistencia es de 0,40 centavos y el de un cable de conexión 
-%es de 0.80 centavos. Además, el número de diodos leds compradas es igual al número de 
-%resistencias más el número de cables de conexión con un igual a 248. ¿Cuántos leds,
+%Para la creaciÃ³n de un cubo de leds de 8x8x8 se adquiriÃ³ un total de 780 unidades entre diodos 
+%leds, resistencias y cables de conexiÃ³n, gastando un total de $249. El precio de un diodo les es 
+%de es de 0.50 centavos, el de una resistencia es de 0,40 centavos y el de un cable de conexiÃ³n 
+%es de 0.80 centavos. AdemÃ¡s, el nÃºmero de diodos leds compradas es igual al nÃºmero de 
+%resistencias mÃ¡s el nÃºmero de cables de conexiÃ³n con un igual a 248. Â¿CuÃ¡ntos leds,
 %resistencias y cables han comprado para realizar el proyecto?
 %siendo estas las ecuaciones: x +y +z =780, 0.16x+0.50y+0.80z=249, x-y-z=248.
 
