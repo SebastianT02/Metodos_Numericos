6faa7cdb6f31495a9cf90ba41f0e773921237b78
pablojoaquin21 <91905894+pablojoaquin21@users.noreply.github.com>
2021-10-28 14:59:54 -0500

    Update main.m

--- Factorizacion_LU/ejemplo_2/main.m
+++ Factorizacion_LU/ejemplo_2/main.m
@@ -3,20 +3,20 @@
 % Fecha: 2021/10/29
 % Descripcion: Se trata de una serie de algoritmos del álgebra lineal
 %para transformar una matriz A como el producto de una multiplicación
-%de dos matrices triangulares U = superior, L =  independiente ,inferior. 
+#de dos matrices triangulares U = superior, L =  independiente ,inferior. 
 
 
-% Problema 1:
-%Para la vida cotidiana para encontrar un sistema de corrientes i1, i2, i3, 
-%cuando se dan los siguientes valores para voltajes, y resistencias
-%E1 = 14V, E2 = 12V, R1 = 6 OHMIOS, R2 = 5 OHMIOS, R3 = 4 OHMIOS 
-%A = [6,0,4; 0,5,4; 1,1, -1];
-%b = [14; 12; 0];
-%
-% 
+% Problema 2:
+%Para la creación de una mini placa de arduino, se adquirió un total 850 unidades
+%entre base_metalica, cables y resistencias gastando un total de $250. El precio
+%de una base metalica es de $1.50, el de cables es de 0.50 centavos c/u, y de una resistencia
+% es de 0.10 cenntavos c/u, además el #de bases mas el # de cables, mas el # de resistencias
+%es igual a 320. ¿cuántas bases,resistencias y cables se han comprado?
+% siendo ecuaciones; x + y + z = 850, 1.5x+0.5y+0.1z=250, x-y-z=320
+
 clc,clear
-A = [6,0,4; 0,5,4; 1,1, -1];
-b = [14; 12; 0];
+A = [1 1 1;1.50,0.50,0.10; 1,-1,-1];
+b = [850; 250; 320];
 
 
 try 
@@ -28,4 +28,6 @@ end
 [L,U] = factorizacion(A)
 fprintf('Solución a la Matriz Ax=b')
 x = sistemaLU(A,b)
+
+%final del main%%
 
