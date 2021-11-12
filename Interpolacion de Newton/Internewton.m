0a85b04b880748601aa616b81999d766f69d4525
Alejandro Romero <91906114+Alejandrop2906@users.noreply.github.com>
2021-11-12 07:52:52 -0500

    Add files via upload

---
+++ Interpolacion de Newton/Internewton.m
@@ -0,0 +1,47 @@
+%   Interpolación polinomial de newton en diferencias divididas
+%   Función que calcula el polinomio obtenido por interpolacion 
+%   de Newton dado unos puntos.
+%   
+%   Parametros-> x,vector: Contiene los puntos en x ; y, vector : 
+%   contiene los puntos en y.
+%   Retorna-> p: Polinomio obtenido por interpolacion de Newton.
+%       
+%   Copyright 2021-2022 Universidad de Cuenca.
+%   $Revision: 0.1 $  $Date: 2021/11/12 $
+%   Built-in function.
+
+function [p]=inter_newton(x,y)
+n=length(x);
+b=zeros(n); %se crea una matriz
+b(:,1)=y(:); %en la 1era columna ingresa los valores de y
+%Comprobación del error
+if size(x)&size(y)<=1
+   msgID='inter_newton condicion: Faltan datos';
+   msg='Error: Los puntos ingresados son incorrectos';
+   #baseException = MException(msgID,msg);
+   #throw(baseException);
+   error(msgID,msg);
+end
+for j=2:n %llena la tabla de diferencias
+    for i=1:n-j+1 %calcula el ultimo punto
+        b(i,j)=(b(i+1,j-1)-b(i,j-1))/(x(i+j-1)-x(i));
+    end
+end
+%construccion del polinomio
+p=num2str(b(1,1));
+xx=x*-1;
+for j=2:n
+    signo='';
+    if b(1,j)>=0
+        Signo='+';
+    end
+    xt='';
+    for i=1:j-1
+        signo2='';
+        if xx(i)>=0
+            signo2='+';
+        end
+        xt=strcat(xt,'*(x',signo2,num2str(xx(i)),')');
+    end
+    p=strcat(p,signo,num2str(b(1,j)),xt);
+end
---
+++ Interpolacion de Newton/mainInternewton2.m
@@ -0,0 +1,28 @@
+% Autores: Alejandro Romero, Pablo Delgado, Sebastian Tacuri.
+%
+% Fecha: 2021/11/12
+% Descripcion: Se trata de una serie de algoritmos del álgebra lineal 
+%para determinar los resultados de un sistema de ecuaciones lineales.
+%
+% Problema 2:
+% El pentóxido de dinitrógeno gaseoso puro reacciona en un reactor intermitente
+% según la reacción estequiométrica. Calcule el polinomio de interpolación de
+% Newton en diferencias divididas para los datos.
+
+clc
+%puntos
+x=[1;-2;2];
+y=[2;4;-2];
+
+try
+  fprintf('La funcion es:\n');
+  p=inter_newton(x,y)
+  ezplot(p);
+  hold on;
+  plot(x,y,'*r')
+  scatter(x,y,100)
+  legend('interpolacion newton','puntos') 
+catch err
+  fprintf('Error: %s\n', err.message);
+  %err.identifier
+end
