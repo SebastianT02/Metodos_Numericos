25e35cfa95b46db7357e8f13856407f9de9f947a
pablojoaquin21 <91905894+pablojoaquin21@users.noreply.github.com>
2022-01-26 13:48:15 -0500

    Create biseccion_main.m

---
+++ Capítulo_1/método_de_bisección/biseccion_main.m
@@ -0,0 +1,33 @@
+% Autores: Pablo Delgado, Alejandro Romero , Mateo Tacuri
+%
+% Fecha: 2021/11/26
+% Descripcion: Se trata de un método que encuentra la raiz de una función
+% por medio de comparaciones y estimaciones, al igual no es exacto el método,
+%puesto tiene una tolerancia admitida.
+
+% Problema 1:
+% Analizar el flujo de tráfico en una red de calles que se cruzan unas con otras.
+% se puede montar un sistema de ecuaciones lineales que, siendo resuelto, mediante
+% los métodos de soluciones de ecuaciones no lineales, permite hacer un cálculo
+% de la cantidad de trafico que va a soportar cada calle y tomar las medidas oportunas
+% (temporizacion) de los semáforos, colocación de pasos cebra y señales de stop
+% para lo cual se da a conocer la función: cos(x)-x^3. Donde el punto inicial es
+% x0 = 0 y el final x1 =1 con la tolerancia de 1e-5 para iniciar, permitiendo identificar
+% máximo de iteraciones.
+
+clc,clear
+
+% Datos para la resolución de método de Bisección
+funcion = 'cos(x)-x^3';
+li = 0;
+ls = 1;
+tolerancia = 1e-5;
+
+% Inicia el main.m
+try
+  fprintf('Solución método de Bisección \n\n')
+  [iteraciones,convergencia,punto_de_raiz]=biseccion(funcion,li,ls,tolerancia)
+catch error
+  fprintf('Error: %s\n',error.message);
+  %err.identifier
+end
