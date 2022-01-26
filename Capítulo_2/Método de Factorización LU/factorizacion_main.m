61c49090d8bdd0a2e3cd1877bec99c49badc732a
pablojoaquin21 <91905894+pablojoaquin21@users.noreply.github.com>
2022-01-26 14:13:30 -0500

    Create factorizacion_main.m

---
+++ Capítulo_2/Método de Factorización LU/factorizacion_main.m
@@ -0,0 +1,31 @@
+% Autores: Pablo Delgado, Alejandro Romero , Mateo Tacuri
+%
+% Fecha: 2021/10/29
+% Descripcion: Se trata de una serie de algoritmos del álgebra lineal
+%para transformar una matriz A como el producto de una multiplicación
+%de dos matrices triangulares U = superior, L =  independiente ,inferior. 
+
+
+% Problema 1:
+%Para la vida cotidiana para encontrar un sistema de corrientes i1, i2, i3, 
+%cuando se dan los siguientes valores para voltajes, y resistencias
+%E1 = 14V, E2 = 12V, R1 = 6 OHMIOS, R2 = 5 OHMIOS, R3 = 4 OHMIOS 
+%A = [6,0,4; 0,5,4; 1,1, -1];
+%b = [14; 12; 0];
+%
+% 
+clc,clear
+A = [6,0,4; 0,5,4; 1,1, -1];
+b = [14; 12; 0];
+
+
+try 
+  [L,U] = factorizacion(A);
+catch err
+  fprintf('Error: %s\n',err.message);
+  %err.identifier
+end
+fprintf('Matriz LU')
+[L,U] = factorizacion(A)
+fprintf('Solución a la Matriz Ax=b')
+x = sistemaLU(A,b)
